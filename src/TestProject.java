55d2e778b776c405eb79becbe757820fb437591f
SFKIT <sherjeel.khalid@gmail.com>
2019-01-17 00:10:18 +0000

    Commit to check if the autobuild setting is working in Jenkins to prove CI

--- src/TestProject.java
+++ src/TestProject.java
@@ -4,6 +4,7 @@ public class TestProject {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HELLO, WORLD!!");
+		System.out.println("Time to test if this auto-build thing works!!");
 
 	}
 
